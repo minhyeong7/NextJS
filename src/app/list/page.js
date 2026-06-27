79611b3419388024583412d3ff5c2d0c703a0f3f
minhyeong7 <163122988+minhyeong7@users.noreply.github.com>
2026-06-27 20:57:48 +0900

    라우팅 설계

---
+++ src/app/list/page.js
@@ -0,0 +1,12 @@
+
+export default function ProductList(){
+    let productName = "상품목록페이지"
+    return(
+        <div>
+            <h1 className="text-red-700">
+                {productName}
+            </h1>
+        </div>
+ 
+    )
+}
